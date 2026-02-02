5c8179f2eef5ab548bc21d8614bb06aaa819a906
masongco <markallensongco@gmail.com>
2026-02-02 09:54:42 +0800

    set React Router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/teamstride-care">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
