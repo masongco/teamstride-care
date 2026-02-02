9991aec18e15e85a52509c8eeac5c6502b7b4ea4
masongco <markallensongco@gmail.com>
2026-02-02 08:58:34 +0800

    resolved vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:"/teamstride-care/",
   server: {
-    base:"/teamstride-care/",
     host: "::",
     port: 8080,
     hmr: {
